e87d7a2f24af82aaf9c417e3aff4d41ecdb39cac
Joaco Esteban <dev.joaco.esteban@gmail.com>
2023-07-13 00:01:28 +0200

    ADD, metas and title

--- app.vue
+++ app.vue
@@ -8,3 +8,21 @@
 <style lang="scss">
 @import 'assets/styles/main';
 </style>
+
+<script setup lang="ts">
+useHead({
+  title: 'SongBasket',
+  meta: [{
+    name: 'description',
+    content: 'SongBasket, The all in one solution to turn Spotify playlists into MP3'
+  },
+  {
+    name: 'charset',
+    content: 'UTF-8'
+  },
+  {
+    name: 'viewport',
+    content: 'width=device-width, initial-scale=1.0'
+  }]
+})
+</script>
