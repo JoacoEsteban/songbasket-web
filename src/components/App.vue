7cef758b903e019316268971193f428b26697757
Joaco Esteban <dev.joaco.esteban@gmail.com>
2023-07-12 21:22:46 +0200

    ADD, gtag

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="app-container df global-center full" :class="{warping}">
-    <div class="block-container text-center df fldc global-center fade-in" :class="{hidden}">
+  <Metas />
+  <div class="app-container df global-center full" :class="{ warping }">
+    <div class="block-container text-center df fldc global-center fade-in" :class="{ hidden }">
       <h1>SongBasket</h1>
       <a class="abs hidden-link" :href="downloadUrl">Download Songbasket</a>
       <button @click="goToDownload">
@@ -11,9 +12,13 @@
 </template>
 
 <script>
+import Metas from './Metas.vue'
 export default {
   name: 'App',
-  data() {
+  components: {
+    Metas
+  },
+  data () {
     return {
       hidden: true,
       warping: false,
@@ -48,49 +53,54 @@ export default {
 </script>
 
 <style lang="scss">
-  @import '../styles/main';
+@import '../styles/main';
 </style>
 <style lang="less" scoped>
-  .app-container {
-    transition: background-color 2s var(--bezier-round-inverted);
-    &.warping {
-      background-color: #fff;
-    }
-  }
+.app-container {
+  transition: background-color 2s var(--bezier-round-inverted);
 
-  .on() {
-    opacity: 1;
-    transform: scale(1);
+  &.warping {
+    background-color: #fff;
   }
-  .off() {
-    opacity: 0;
-    transform: scale(.9);
-  }
+}
 
-  .hidden-link {
-    opacity: 0;
-    pointer-events: none;
-  }
+.on() {
+  opacity: 1;
+  transform: scale(1);
+}
 
-  .block-container {
-    @transition: 3s var(--bezier-chill);
-    transition: opacity @transition, transform @transition;
-    &.hidden {
-      transition-timing-function: var(--bezier-round-inverted);
-      transition-duration: .5s;
-      .off();
-    }
+.off() {
+  opacity: 0;
+  transform: scale(.9);
+}
+
+.hidden-link {
+  opacity: 0;
+  pointer-events: none;
+}
+
+.block-container {
+  @transition: 3s var(--bezier-chill);
+  transition: opacity @transition, transform @transition;
+
+  &.hidden {
+    transition-timing-function: var(--bezier-round-inverted);
+    transition-duration: .5s;
+    .off();
   }
+}
+
+h1 {
+  font-size: 3em;
+  margin-bottom: .2em;
+}
+
+@media (max-width: 450px) {
   h1 {
-    font-size: 3em;
-    margin-bottom: .2em;
+    font-size: 16vw;
   }
-  @media (max-width: 450px) {
-    h1 {
-      font-size: 16vw;
-    }
-    button {
-      font-size: 5vw;
-    }
+
+  button {
+    font-size: 5vw;
   }
-</style>
+}</style>
